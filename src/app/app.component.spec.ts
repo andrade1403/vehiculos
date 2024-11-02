41f0e60023f9d057ca340fdae09f7d52fdb906c9
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2024-11-02 10:33:08 -0500

    Desarrollo punto 3 de pruebas del componente.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { VehiclesListComponent } from './vehicles/vehicles-list/vehicles-list.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterModule.forRoot([])
+        RouterTestingModule, HttpClientModule
       ],
       declarations: [
-        AppComponent
+        AppComponent, VehiclesListComponent
       ],
     }).compileComponents();
   });
@@ -30,6 +33,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, vehiculos');
+    expect(compiled.querySelector('h1')?.textContent).toContain('TuSegundazo.com');
   });
 });
--- src/app/vehicles/vehicles-list/vehicles-list.component.spec.ts
+++ src/app/vehicles/vehicles-list/vehicles-list.component.spec.ts
@@ -1,16 +1,20 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { VehiclesListComponent } from './vehicles-list.component';
+import { fa, faker } from '@faker-js/faker';
+import { Vehicle } from '../vehicle';
 
 describe('VehiclesListComponent', () => {
   let component: VehiclesListComponent;
   let fixture: ComponentFixture<VehiclesListComponent>;
+  let debug: DebugElement;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [ VehiclesListComponent ]
     })
     .compileComponents();
@@ -19,10 +23,42 @@ describe('VehiclesListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(VehiclesListComponent);
     component = fixture.componentInstance;
+
+    //Realizamos ciclo para generasr datos con faker
+    for(let i = 0; i < 3; i++) {
+      const vehiculo = new Vehicle(
+        faker.number.int(),
+        faker.lorem.word(),
+        faker.lorem.word(),
+        faker.lorem.word(),
+        faker.number.int(),
+        faker.number.int(),
+        faker.lorem.word(),
+        faker.image.url()
+      );
+      component.vehicles.push(vehiculo);
+      console.log(vehiculo);
+    }
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have table', () => {
+    const table = debug.query(By.css('table'));
+    expect(table).toBeTruthy();
+  });
+
+  it('should have table head', () => {
+    const tableHead = debug.query(By.css('thead'));
+    expect(tableHead).toBeTruthy();
+  })
+
+  it('should have 3 rows plus header', () => {
+    expect(debug.queryAll(By.css('tr'))).toHaveSize(4);
+  });
+
 });
