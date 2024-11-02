7ec22d949c6d3836da7a1acae7c17dcb38c1dd08
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2024-11-02 07:31:44 -0500

    Creamos clase vehicle.

---
+++ src/app/vehicles/vehicle.ts
@@ -0,0 +1,6 @@
+export class Vehicle {
+    //Metodo construcutor de la clase
+    constructor(public id: number, public marca: string, public linea: string,
+                public referencia: string, public modelo: number, public kilometraje: number,
+                public color: string, public imagen: string) { }
+}
