20a0ebd05cc80a88ab033f53b5b62538249406ff
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2024-11-02 09:43:59 -0500

    Desarrollo del punto 2.

--- src/app/vehicles/vehicles-list/vehicles-list.component.ts
+++ src/app/vehicles/vehicles-list/vehicles-list.component.ts
@@ -11,6 +11,10 @@ export class VehiclesListComponent implements OnInit {
   //Declaramos arreglo de vehiculos
   vehicles: Array<Vehicle> = [];
 
+  //Declaramos arreglo de marcas
+  marcas: Array<string> = [];
+  dict: {[key: string]: number} = {};
+
   //Constructor de la clase
   constructor(private vehicleService: VehiclesService) { }
 
@@ -18,7 +22,28 @@ export class VehiclesListComponent implements OnInit {
   getVehicles() {
     this.vehicleService.getVehicles().subscribe(vehicles => {
       this.vehicles = vehicles;
+      this.vehicleCount();
     })
+  }
+
+  //Metodo para traer las marcas unicas
+  getUniqueBrands() {
+    this.marcas = Array.from(new Set(this.vehicles.map(v => v.marca)));
+  }
+
+  //Metodo para contar carros por marca
+  vehicleCount(): void {
+    //Traemos las marcas unicas
+    this.getUniqueBrands();
+
+    //Inicilizamos el diccionario con las marcas
+    for(let m of this.marcas) { this.dict[m] = 0; }
+
+    //Contamos los vehiculos por marca
+    for(let v of this.vehicles) {
+      let marca = v.marca;
+      this.dict[marca] += 1;
+    }
   }
 
   ngOnInit() {
